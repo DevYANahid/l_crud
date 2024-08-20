6a39f106f9da47e5c7dc4dd88c3aadb8d5d50192
DevYANahid <codealgorithmschool@gmail.com>
2024-08-20 15:26:25 +0600

    added data

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -37,7 +37,11 @@
     </thead>
     <tbody>
       <tr>
-        
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+        <td>
           <a href="" class="btn btn-primary">Edit</a>
           <a href="" class="btn btn-danger">Delete</a>
         </td>
